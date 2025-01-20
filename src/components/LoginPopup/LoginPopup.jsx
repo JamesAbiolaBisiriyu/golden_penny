ef881a90068bba49654796b473d05a691c656a0a
james <emperor2g@gmail.com>
2025-01-20 18:40:37 +0100

    2:42

--- src/components/LoginPopup/LoginPopup.jsx
+++ src/components/LoginPopup/LoginPopup.jsx
@@ -4,18 +4,34 @@ import './LoginPopup.css'
 import {assets} from '../../assets/assets'
 
 
-const LoginPopup = () => {
+const LoginPopup = ({setShowLogin}) => {
 
-const [currState,setCurrState] = useState("Sign Up");
+const [currState,setCurrState] = useState("Login");
 
 
   return (
     <div className='login-popup'>
       <form action="" className="login-popup-container">
         <div className="login-popup-title">
+          
           <h2>{currState}</h2>
-          <img onClick={()=>{setShowLogin}} src={assets.cross_icon} alt="" />
+          <img onClick={()=>{setShowLogin(false)}} src={assets.cross_icon} alt="" />
         </div>
+        <div className="login-popup-input">
+          {currState==="Login"?<></>:<input type="text" placeholder='Your name' required />}
+          <input type="email" placeholder='Your email' required />
+          <input type="password" placeholder='Password' required />
+        </div>
+        <button>{currState==="Sign Up"?"Create Account":"Login"}</button>
+        <div className="login-popup-condition">
+          <input type="checkbox" required />
+          <p>By Continuing, I agree to the Terms of use & privacy policy.</p>
+        </div>
+        {currState==="Login"
+        ?<p>Create a new account? <span onClick={()=>setCurrState("Sign Up")}>Click here</span></p>
+        :<p>Already Have An Account? <span onClick={()=>setCurrState("Login")}>Login Here</span></p>
+        }
+        
       </form>
     </div>
   )
