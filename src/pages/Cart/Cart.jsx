ba8cb1d715a0854e0701fbd60b1150abfa7ea21d
james <emperor2g@gmail.com>
2025-01-20 21:32:27 +0100

    2:47

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,15 +1,12 @@
-import React, { useContext } from 'react'
-import './Cart.css'
-import {StoreContext} from '../../context/StoreContext'
+import { useContext } from "react";
+import "./Cart.css";
+import { StoreContext } from "../../context/StoreContext";
 
 const Cart = () => {
-
-  const {cartItems, food_list, removeFromCart} = useContext(StoreContext);
-
-
+  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
 
   return (
-    <div className='cart'>
+    <div className="cart">
       <div className="cart-items">
         <div className="cart-items-title">
           <p>Items</p>
@@ -21,9 +18,26 @@ const Cart = () => {
         </div>
         <br />
         <hr />
+        {food_list.map((item, index) => {
+          if (cartItems[item._id] > 0) {
+            return (
+              <div>
+                <div className="cart-items-title cart-items-item">
+                  <img src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>{item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>{item.price * cartItems[item._id]}</p>
+                  <p>x</p>
+                </div>
+                <hr />
+              </div>
+            );
+          }
+        })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
